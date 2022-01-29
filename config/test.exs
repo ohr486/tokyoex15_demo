15f910ff646ab68cade25633a9b5e3ad38cb828c
ohr486 <ohr486@gmail.com>
2022-01-30 03:56:56 +0900

    use test db host from env

--- config/test.exs
+++ config/test.exs
@@ -8,7 +8,7 @@ import Config
 config :demo, Demo.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
+  hostname: "#{System.get_env("MIX_TEST_DB_HOST")}",
   database: "demo_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
